fa26c633262dfd989770a8bda4f94ea0598c23e2
dnfwlxo11 <dnfwlxo11@naver.com>
2021-09-01 15:25:50 +0900

    edit: dependency code change

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,26 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+    /*
+     * 이렇게 구현체에 의존되게 만들면 안된다. (DIP 위반)
+     * 아래와 같이 확장 시 코드를 변경하게 만들면 안된다. (OCP 위반)
+     */
+//     private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//     private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,33 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되야함")
+    void vip_o() {
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        int discount = discountPolicy.discount(member, 10000);
+
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("VIP가 아니면 적용되면 안됨")
+    void vip_x() {
+        Member member = new Member(1L, "memberBASIC", Grade.BASIC);
+
+        int discount = discountPolicy.discount(member, 10000);
+
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+}
